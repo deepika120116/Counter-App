25cf83bfe6968603584ac0472a297b691dde6115
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-02-10 17:25:41 -0500

    committing the changes in the file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import NavBar from './components/navBar';
 import './App.css';
+import Counters from './components/counters';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+   state = {
+        counters: [
+        { id: 1, value: 4 },
+        {id:2, value:0},
+        {id:3, value:0},
+        {id:4, value:0}]
+   };
+  constructor(props){
+    super(props);
+    console.log("Constructor is called", this.props.value);
+    // Inorder to use this.props, props arguments should be passed in the constrcutor & also super functions
+  }
+  componentDidMount(){
+  //this is for Ajax call to get the data from the server
+    console.log("App-mounted");
+  }
+    handleDelete=(counterId)=> {
+        //console.log("Handle event called from counter component",counterId);
+        const counterVar = this.state.counters.filter(counter => counter.id !== counterId);//creating new array without the id which has been deleted
+        this.setState({ counters: counterVar });//overriding counters array with countervariable (new array)
+        
+    }
+    handleReset = () => {
+        const counterReset = this.state.counters.map(counter => {
+            counter.value = 0;
+            return counter;
+        });
+        this.setState({ counters: counterReset });
+    }
+    handleIncrement = counter => {
+        console.log(counter);
+        const counterIncrement = [...this.state.counters];
+        const index = counterIncrement.indexOf(counter);
+        counterIncrement[index] = { ...counter }; 
+        counterIncrement[index].value++;
+        this.setState({ counters: counterIncrement });
+
+    }
+   handleDecrement = counter => {
+        console.log(counter);
+        const counterDecrement = [...this.state.counters];
+        const index = counterDecrement.indexOf(counter);
+        counterDecrement[index] = { ...counter }; 
+        counterDecrement[index].value--;
+        this.setState({ counters: counterDecrement });
+
+    }
+  render() {
+    console.log("App-Render method is called");
+    return (
+      <React.Fragment>
+        <NavBar totalCounters={ this.state.counters.filter(counter=>(counter.value>0)).length} />
+          
+        <main className='container'>
+          <Counters counters={this.state.counters } onReset={this.handleReset} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete} />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
